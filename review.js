21b1c5dd459449b40e7e0c5c184d2ca48407ec1b
Oysters1979 <yuerutu@gmail.com>
2019-05-12 22:28:05 +0900

    ty

--- review.js
+++ review.js
@@ -1,3 +1,4 @@
+//config of firebase
 var config = {
     apiKey: "AIzaSyC8IHBEyIwBLsqE80vYcW_rvETrcdSEuzg",
     authDomain: "logkids.firebaseapp.com",
@@ -9,8 +10,126 @@ var config = {
 
 firebase.initializeApp(config);//initialize firebase
 
-// var name_idx;
-// var kid_key;
+ var name_idx;
+ var kid_key;
+ //initialize 在click reveiw之后执行
+function initialize(){
+    firebase.database().ref('/name_index/').once('value', function(snapshot){
+        var myValue = snapshot.val();
+        if(myValue!==null){
+            console.log("getting chosen kid name");
+            name_idx=myValue.name_index;
+            document.getElementById("head_name").innerHTML=name_idx;//顶部显示的名字
+            firebase.database().ref('/kidsBox/').once('value', function(snapshot){
+                var myValue = snapshot.val();
+                if(myValue!==null){
+                    console.log("locating kid");
+                    var keyList = Object.keys(myValue);
+                    for(var i=0;i<keyList.length;i++){
+                        var myKey = keyList[i];
+                        if(name_idx===myValue[myKey].name){
+                            kid_key=myKey;
+                            break;
+                        }
+                    }
+                    //后续函数
+                    read_log_inTime_range(selected_category,1 )//第二个变量为 总共需要遍历的log数量
+
+
+                }
+            });
+        }
+    });
+}
+
+var category_menu = document.getElementById('category_select_menu');
+var selected_category; 
+
+var range_radios = document.getElementsByName('customRadioInline1');
+var selected_range='';
+
+
+
+
+$("input[name='customRadioInline1']").click(function(){
+
+    if(range_radios[0].checked){
+        selected_range='Recent one week';
+    }
+    else if(range_radios[1].checked){
+        selected_range='Recent one month';
+    }
+
+
+});
+
+var statistic_tag=[];
+function read_log_inTime_range(category="",total_logs=0){
+    firebase.database().ref('/kidsBox/'+kid_key+'/logBox/'+category+"/").once('value', function(snapshot){
+        var myValue = snapshot.val();
+        if(myValue!==null){
+            console.log("loading logs in time selected: "+category);
+            var keyList=Object.keys(myValue);
+            for(var i=keyList.length-1;i>keyList.length-total_logs-1;i--){
+                console.log(i);
+                var myKey=keyList[i];
+                console.log(myKey);
+                var comments=myValue[myKey].comment;
+                var time=myValue[myKey].time;
+                var tags=myValue[myKey].tag;
+                var important=myValue[myKey].important;
+                console.log(comments);
+                add_one_log_box(time,tags,important,comments);
+            }
+        }
+    });
+}
+
+function add_one_log_box(time="",tags=[],important=false,com=""){
+    var div1=document.createElement("div");
+    div1.className="row";
+    var div2=document.createElement("div");
+    div2.className="col-md-12 text-center";
+
+    var i_1 = document.createElement("i");
+    i_1.className="fas fa-comment-dots fa-lg";
+
+    var h5= document.createElement("h5");
+    h5.innerHTML=com;
+    
+    var p1=document.createElement("p");
+    p1.innerHTML="<i class='fas fa-star'></i> important";
+
+    var p2=document.createElement("p")
+    p2.className="time_text";
+    p2.innerHTML="<small class='text-muted'><i>"+time+"</i></small>";
+
+    var p3=document.createElement("p");
+    var tagstr="";
+    for (var i=0;i<tags.length;i++){
+      statistic_tag.push(tags[i]);
+      tagstr+=tags[i];
+      if(i+1!==tags.length){
+        tagstr+=", ";
+      }
+    }
+
+    p3.innerHTML="<small class='text-muted'> tags:"+tagstr+"</small>";
+
+    div2.appendChild(i_1);
+    div2.appendChild(h5);
+    if(important===true){
+      div2.appendChild(p1);
+    }
+    div2.appendChild(p2);
+    div2.appendChild(p3);
+    div1.appendChild(div2);
+
+    $(div1).insertAfter("#category_title");
+
+}
+
+
 
 //chart
 var ctxP = document.getElementById("eating-pieChart1").getContext('2d');
@@ -48,30 +167,15 @@ var ctxP = document.getElementById("eating-pieChart1").getContext('2d');
 
 //start program
 
-var category_menu = document.getElementById('category_select_menu');
-var selected_category='';
-
-var range_radios = document.getElementsByName('customRadioInline1');
-var selected_range='';
-
-
-$("input[name='customRadioInline1']").click(function(){
-
-    if(range_radios[0].checked){
-        selected_range='Recent one week';
-    }
-    else if(range_radios[1].checked){
-        selected_range='Recent one month';
-    }
-    else if(range_radios[2].checked){
-        selected_range='Recent three months';
-    }
-
-});
 
 
 
-// var reviewbutton = document.getElementById("review_button");
+
+var reviewbutton = document.getElementById("review_btn");
+reviewbutton.onclick=function(){
+  selected_category=$("#category_select_menu option:selected").val();
+  initialize();
+}
 // createbutton.onclick = function(){
 
 //     setTimeout("window.location.href='./Review Past Data.html'",950)
